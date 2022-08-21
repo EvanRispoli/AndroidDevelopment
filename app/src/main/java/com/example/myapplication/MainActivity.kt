89c2be8814e61369e72af95e4a7f02427319695e
Evan Rispoli <evan.rispoli@gmail.com>
2022-08-21 19:12:31 -0400

    Creates new views for mouse price and keyboard price. Creates a function to set the prices
    Creates a buy button to calculate the total
    Creates a view to show the total price
    Creates a function to calculate the total price

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,14 +10,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnPlus: Button
     private lateinit var btnMinus: Button
     private lateinit var tvMouseQty: TextView
+    private lateinit var tvMousePrice: TextView
 
     private lateinit var btnPlusKeyboard: Button
     private lateinit var btnMinusKeyboard: Button
     private lateinit var tvKeyboardQty: TextView
+    private lateinit var tvKeyboardPrice: TextView
+
     //lateinit don't define a value immediately, but it causes an error if we start the app
 
     private var mouseQty: Int = 0
+    private var mousePrice: Double = 20.55
+
     private var KeyboardQty: Int = 0
+    private var KeyboardPrice: Double = 39.99
+
+    private lateinit var btnCloseDeal : Button
+    private lateinit var tvTotal: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,18 +39,32 @@ class MainActivity : AppCompatActivity() {
     private fun settings() {
         viewsStart()
         setButtons()
+        setPrices()
 
     }
 
+    private fun setPrices() {
+
+        tvMousePrice.text = mousePrice.toString()
+        tvKeyboardPrice.text = KeyboardPrice.toString()
+    }
+
     private fun viewsStart() {
+        //If we use the lateinit, we have to declare the values before start the app.
         btnPlus = findViewById(R.id.btn_plus)  //represents the btn_plus on screen
         btnMinus = findViewById(R.id.btn_minus) //represents the btn_minus on screen
         tvMouseQty = findViewById(R.id.tv_mouse_qty) //represents the tv_mouse_qty on screen
+        tvMousePrice = findViewById(R.id.mousePrice)
 
         btnPlusKeyboard = findViewById(R.id.btn_plus_Keyboard)
         btnMinusKeyboard = findViewById(R.id.btn_minus_Keyboard)
         tvKeyboardQty = findViewById(R.id.tv_Keyboard_qty)
-        //If we use the lateinit, we have to declare the values before start the app.
+        tvKeyboardPrice = findViewById(R.id.keyboardPrice)
+
+        tvTotal = findViewById(R.id.tv_total)
+        btnCloseDeal = findViewById(R.id.btn_deal)
+
+
     }
 
     private fun setButtons() {
@@ -56,6 +79,9 @@ class MainActivity : AppCompatActivity() {
         }
         btnMinusKeyboard.setOnClickListener {
             keyboardSub()
+        }
+        btnCloseDeal.setOnClickListener{
+            closeDeal()
         }
     }
 
@@ -85,6 +111,12 @@ class MainActivity : AppCompatActivity() {
             KeyboardQty--
         }
         tvKeyboardQty.text = KeyboardQty.toString()
+    }
+
+    private fun closeDeal(){
+        val total = mouseQty * mousePrice + KeyboardQty * KeyboardPrice
+        tvTotal.text = "Total: $ ${"%.2f".format( total ) }"
+
     }
 
 
