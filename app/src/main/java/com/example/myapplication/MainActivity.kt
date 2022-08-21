7efa9e4be709a035ea8945a37ff651b8ee4b6481
Evan Rispoli <evan.rispoli@gmail.com>
2022-08-20 22:41:28 -0400

    Creates the new function to increment and decrement the keyboard qty value

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,9 +10,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnPlus: Button
     private lateinit var btnMinus: Button
     private lateinit var tvMouseQty: TextView
+
+    private lateinit var btnPlusKeyboard: Button
+    private lateinit var btnMinusKeyboard: Button
+    private lateinit var tvKeyboardQty: TextView
     //lateinit don't define a value immediately, but it causes an error if we start the app
 
     private var mouseQty: Int = 0
+    private var KeyboardQty: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +37,10 @@ class MainActivity : AppCompatActivity() {
         btnPlus = findViewById(R.id.btn_plus)  //represents the btn_plus on screen
         btnMinus = findViewById(R.id.btn_minus) //represents the btn_minus on screen
         tvMouseQty = findViewById(R.id.tv_mouse_qty) //represents the tv_mouse_qty on screen
+
+        btnPlusKeyboard = findViewById(R.id.btn_plus_Keyboard)
+        btnMinusKeyboard = findViewById(R.id.btn_minus_Keyboard)
+        tvKeyboardQty = findViewById(R.id.tv_Keyboard_qty)
         //If we use the lateinit, we have to declare the values before start the app.
     }
 
@@ -41,6 +50,12 @@ class MainActivity : AppCompatActivity() {
         }
         btnMinus.setOnClickListener {
             mouseSub()
+        }
+        btnPlusKeyboard.setOnClickListener {
+            keyboardAdd()
+        }
+        btnMinusKeyboard.setOnClickListener {
+            keyboardSub()
         }
     }
 
@@ -56,6 +71,20 @@ class MainActivity : AppCompatActivity() {
             mouseQty--
         }
         tvMouseQty.text = mouseQty.toString()
+    }
+
+    //Creates a function to increment the keyboard qty
+    private fun keyboardAdd() {
+        KeyboardQty++
+        tvKeyboardQty.text = KeyboardQty.toString()
+    }
+
+    //Creates a function to decrement the mouse qty
+    private fun keyboardSub() {
+        if (KeyboardQty > 0) {
+            KeyboardQty--
+        }
+        tvKeyboardQty.text = KeyboardQty.toString()
     }
 
 
